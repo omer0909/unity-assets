d042dccaedc41c7b25180020bbc9d93bdd3ec7f4
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-15 00:32:26 +0300

    Update and rename fpsCharachter.cs to fpsCharacter.cs

--- fpsCharacter.cs
+++ fpsCharacter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fpsCharachter : MonoBehaviour
+public class fpsCharacter : MonoBehaviour
 {
     
     private bool ground=false;
