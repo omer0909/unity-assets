42ad85e1b7a3ad84f6065a3a998f77a5368f9bdf
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-14 19:39:26 +0300

    carController

---
+++ carController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] whweels;
+    public float motorTorque=500;
+    public float brakeTorque=1000;
+    public float angle=30;
+    [SerializeField]
+    private WheelCollider[] whweelColliders;
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        bool breake=Input.GetKey(KeyCode.Space);
+        
+        for(int i=0;i<whweelColliders.Length;i++){
+            Vector3 pos;
+            Quaternion rot;
+            whweelColliders[i].GetWorldPose(out pos,out rot);
+            whweels[i].rotation=rot;
+            whweels[i].position=pos;
+
+            whweelColliders[i].motorTorque=motorTorque*Input.GetAxis("Vertical");
+            whweelColliders[i].brakeTorque=brakeTorque*Input.GetAxis("Jump");
+            if(i<2){
+                whweelColliders[i].steerAngle=angle*Input.GetAxis("Horizontal");
+            }
+        }
+    }
+}
