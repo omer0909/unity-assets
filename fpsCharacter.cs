f1d08c7761a2c9acb2911c6047602723f91c5c26
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-17 23:19:13 +0300

    Update fpsCharacter.cs

--- fpsCharacter.cs
+++ fpsCharacter.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class fpsCharacter : MonoBehaviour
 {
+    public bool touchScreen=false;
     private float velocity=0;
     private bool ground=false;
     private bool ceiling=false;
     public float jumpMultiply=5;
     public bool invertY=false;
-     public float walkSpeed=5;
-     public float runSpeed=10;
-     public float cameraSensivity=3;
-     private CharacterController charachter;
-     private Transform fpsCamera;
-     public Vector2 moveInput=Vector2.zero;
-     public Vector2 cameraInput=Vector2.zero;
-     private void Awake() {
+    public float walkSpeed=5;
+    public float runSpeed=10;
+    public float cameraSensivity=3;
+    private CharacterController charachter;
+    private Transform fpsCamera;
+    private Vector2 moveInput=Vector2.zero;
+    private Vector2 cameraInput=Vector2.zero;
+    private void Awake() {
 
         charachter=GetComponent<CharacterController>();
 
@@ -25,7 +26,7 @@ public class fpsCharacter : MonoBehaviour
         Cursor.visible=false;
         fpsCamera=Camera.main.transform;
         charachter.minMoveDistance=0;
-     }
+    }
     private void cameraDirection(){
         Vector2 cameraOld=fpsCamera.eulerAngles;
         if(invertY){
@@ -36,6 +37,11 @@ public class fpsCharacter : MonoBehaviour
     }
     void isInput(){
         cameraInput=new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"));
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            jump();
+        }
+        
 
         if(Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.S)){
             moveInput.y=0;
@@ -57,6 +63,25 @@ public class fpsCharacter : MonoBehaviour
             moveInput.x=0;
         }
     }
+    void tochInput(){
+        bool cameraA=true;
+        bool moveA=true;
+        foreach(Touch touch in Input.touches){
+            if(touch.rawPosition.x>Screen.width/2){
+                cameraInput=touch.deltaPosition/Screen.height*100;
+                cameraA=false;
+            }else{
+                moveInput=(touch.position-touch.rawPosition)/Screen.height*6;
+                moveA=false;
+            }
+        }
+        if(cameraA){
+            cameraInput=Vector2.zero;
+        }
+        if(moveA){
+            moveInput=Vector2.zero;
+        }
+    }
     void physics(){
 
         if(ground&&velocity<0){
@@ -70,10 +95,10 @@ public class fpsCharacter : MonoBehaviour
 
     private void move(){
 
-        float speed=(Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.RightShift))? runSpeed: walkSpeed;
+        float speed=(Input.GetKey(KeyCode.LeftShift)||touchScreen)? runSpeed: walkSpeed;
         Vector2 direction=new Vector2(Mathf.Sin(fpsCamera.eulerAngles.y*Mathf.Deg2Rad),Mathf.Cos(fpsCamera.eulerAngles.y*Mathf.Deg2Rad));
         Vector2 control=Vector2.ClampMagnitude(moveInput,1);
-
+        
         Vector2 x=control.y*direction;
         Vector2 y=-control.x*new Vector2(-direction.y,direction.x);
         Vector2 go=(x+y)*speed;
@@ -87,23 +112,29 @@ public class fpsCharacter : MonoBehaviour
             charachter.Move(new Vector3(go.x,velocity,go.y)*Time.deltaTime);
         }
     }
-    void jump(){
-        if(Input.GetKeyDown(KeyCode.Space)&&ground){
+    public void jump(){
+        if(ground){
             velocity=jumpMultiply;
         }
     }
     
     private void isGround(){
         ground=charachter.isGrounded;
-        RaycastHit hit;
-        ceiling=(Physics.Raycast( transform.position,Vector3.up,out hit,0.1f+charachter.height*0.5f));
+        ceiling=(charachter.collisionFlags & CollisionFlags.Above) != 0;
     }
+
     void Update()
     {
-        isInput();
+        
         isGround();
+
+        if(touchScreen){
+            tochInput();
+        }else{
+            isInput();
+        }
+
         cameraDirection();
-        jump();
         physics();
         move();
     }
