b6f3b82e4339de386d47ba863cb9990e1204fbdb
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-18 06:23:29 +0300

    Update fpsCharacter.cs

--- fpsCharacter.cs
+++ fpsCharacter.cs
@@ -26,6 +26,7 @@ public class fpsCharacter : MonoBehaviour
         Cursor.visible=false;
         fpsCamera=Camera.main.transform;
         charachter.minMoveDistance=0;
+        //charachter.slopeLimit=90;
     }
     private void cameraDirection(){
         Vector2 cameraOld=fpsCamera.eulerAngles;
