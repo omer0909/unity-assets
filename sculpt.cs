4430d9b6f4fbe617d27c78396781b950e0462f87
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-21 02:56:30 +0300

    Update sculpt.cs

--- sculpt.cs
+++ sculpt.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 public class sculpt : MonoBehaviour
 {
     private Vector2 oldMousePos;
+    private float distance;
     public float size=0.4f;
     public float force=0.1f;
     public float spacing=100;
@@ -16,7 +17,12 @@ public class sculpt : MonoBehaviour
     public AnimationCurve curve;
     private Mesh mesh;
     private MeshCollider mCollider;
+    private Camera cam;
+    float distanceMultiplay;
     void Awake() {
+        cam=Camera.main;
+        distanceMultiplay=Mathf.Sin(cam.fieldOfView*0.5f*Mathf.Deg2Rad);
+        
         if (curve.length==0){
             curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
             curve.preWrapMode = WrapMode.PingPong;
@@ -40,6 +46,7 @@ public class sculpt : MonoBehaviour
     }
     void Update()
     {
+        
         if(Input.GetKeyDown(KeyCode.LeftControl)){
             force*=-1;
         }
@@ -50,12 +57,13 @@ public class sculpt : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
             oldMousePos=Input.mousePosition;
 
-            Ray ray=Camera.main.ScreenPointToRay(oldMousePos);
+            Ray ray=cam.ScreenPointToRay(oldMousePos);
             RaycastHit hit;
-            if(Physics.Raycast(ray,out hit)){       
+            if(Physics.Raycast(ray,out hit)){
                 point(hit.point,hit.normal);
                 mesh.RecalculateNormals();
                 mCollider.sharedMesh=mesh;
+                distance=hit.distance*distanceMultiplay;
             }
         }
 
@@ -63,19 +71,20 @@ public class sculpt : MonoBehaviour
             Vector2 mousepos=Input.mousePosition;
             Vector2 mousedelta=mousepos-oldMousePos;
 
-            Transform camera=Camera.main.transform;
+            Transform camera=cam.transform;
             Vector3 mouseMove=camera.right*mousedelta.x+camera.up*mousedelta.y;
             mouseMove.Normalize();
             
             while(true){
                 if(Vector2.Distance(mousepos,oldMousePos)>size*spacing){
-                    oldMousePos=Vector2.MoveTowards(oldMousePos,mousepos,size*spacing*slope);
+                    oldMousePos=Vector2.MoveTowards(oldMousePos,mousepos,(size*spacing*slope)/distance);
                     
-                    Ray ray=Camera.main.ScreenPointToRay(oldMousePos);
+                    Ray ray=cam.ScreenPointToRay(oldMousePos);
                     RaycastHit hit;
                     if(Physics.Raycast(ray,out hit)){
                         slope=1-Mathf.Abs(Vector3.Dot(hit.normal,mouseMove));
                         point(hit.point,hit.normal);
+                        distance=hit.distance*distanceMultiplay;
                     }
 
                 }else{
