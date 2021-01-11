ae843195e7a7ebd6d9cb5dbe20d89df78667a227
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2021-01-11 06:07:43 +0300

    Update wheel.cs

--- snow/wheel.cs
+++ snow/wheel.cs
@@ -15,7 +15,6 @@ public class wheel : MonoBehaviour
     {
         if(wheelCollider.GetGroundHit(out WheelHit hit)){
             if(hit.collider.tag=="snow"){
-                //hit.collider.gameObject.GetComponent<ground>().edit(hit.point,-hit.normal,trailRadius);
                 if(Vector3.Distance(oldpos,hit.point)>trailRadius*0.5f){
                     hit.collider.gameObject.GetComponent<createSnow>().edit(new Vector2(hit.point.z,hit.point.x),trailRadius);
                     oldpos=hit.point;
