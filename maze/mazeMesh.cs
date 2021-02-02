2aa4fe27d287041f5e6c8f65fdf5db687862e80d
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2021-02-02 17:04:03 +0300

    Update mazeMesh.cs

--- maze/mazeMesh.cs
+++ maze/mazeMesh.cs
@@ -16,8 +16,8 @@ public class mazeMesh : MonoBehaviour
 
         List<Vector2Int> way = new List<Vector2Int>();
 
-        way.Add(Vector2Int.zero);
-        mazeWall[0, 0] = true;
+        way.Add(Vector2Int.one);
+        mazeWall[1, 1] = true;
 
         int index = 0;
         int maxIndex = 0;
@@ -69,155 +69,195 @@ public class mazeMesh : MonoBehaviour
         {
             for (int y = 0; y < matrix; y++)
             {
-                if (mazeWall[x, y] == true)
+
+                if (!mazeWall[x, y])
                 {
+                    bool back = false, forward = false, left = false, right = false;
+
                     if (y + 1 < matrix)
                     {
-                        if (!mazeWall[x, y + 1])
+                        if (mazeWall[x, y + 1])
                         {
-                            for (int i = 0; i < 4; i++)
-                            {
-                                normals.Add(Vector3.back);
-                            }
-                            Vector3[] PlaneV ={
-                                new Vector3(x+0.5f,1,y+0.5f),
-                                new Vector3(x-0.5f,1,y+0.5f),
-                                new Vector3(x+0.5f,0,y+0.5f),
-                                new Vector3(x-0.5f,0,y+0.5f)
-                            };
-                            vertices.AddRange(PlaneV);
-                            int[] PlaneT ={
-                                trisIndex,trisIndex+3,trisIndex+1,
-                                trisIndex,trisIndex+2,trisIndex+3
-                            };
-                            tris.AddRange(PlaneT);
-                            trisIndex += 4;
-                            Vector2[] PlaneU ={
-                                new Vector2(1,1),
-                                new Vector2(0,1),
-                                new Vector2(1,0),
-                                new Vector2(0,0)
-                            };
-                            uv.AddRange(PlaneU);
+                            back = true;
                         }
                     }
+                    else
+                    {
+                        back = true;
+                    }
+
                     if (y > 0)
                     {
-                        if (!mazeWall[x, y - 1])
+                        if (mazeWall[x, y - 1])
                         {
-                            for (int i = 0; i < 4; i++)
-                            {
-                                normals.Add(Vector3.forward);
-                            }
-                            Vector3[] PlaneV ={
+                            forward = true;
+                        }
+                    }
+                    else
+                    {
+                        forward = true;
+                    }
+
+                    if (x + 1 < matrix)
+                    {
+                        if (mazeWall[x + 1, y])
+                        {
+                            left = true;
+                        }
+                    }
+                    else
+                    {
+                        left = true;
+                    }
+
+                    if (x > 0)
+                    {
+                        if (mazeWall[x - 1, y])
+                        {
+                            right = true;
+                        }
+                    }
+                    else
+                    {
+                        right = true;
+                    }
+
+                    if (forward)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            normals.Add(Vector3.back);
+                        }
+                        Vector3[] PlaneV ={
                                 new Vector3(x+0.5f,1,y-0.5f),
                                 new Vector3(x-0.5f,1,y-0.5f),
                                 new Vector3(x+0.5f,0,y-0.5f),
                                 new Vector3(x-0.5f,0,y-0.5f)
                             };
-                            vertices.AddRange(PlaneV);
-                            int[] PlaneT ={
-                                trisIndex,trisIndex+1,trisIndex+3,
-                                trisIndex,trisIndex+3,trisIndex+2
+                        vertices.AddRange(PlaneV);
+                        int[] PlaneT ={
+                                trisIndex,trisIndex+3,trisIndex+1,
+                                trisIndex,trisIndex+2,trisIndex+3
                             };
-                            tris.AddRange(PlaneT);
-                            trisIndex += 4;
-                            Vector2[] PlaneU ={
+                        tris.AddRange(PlaneT);
+                        trisIndex += 4;
+                        Vector2[] PlaneU ={
                                 new Vector2(1,1),
                                 new Vector2(0,1),
                                 new Vector2(1,0),
                                 new Vector2(0,0)
                             };
-                            uv.AddRange(PlaneU);
-                        }
+                        uv.AddRange(PlaneU);
                     }
-                    if (x + 1 < matrix)
+
+                    if (back)
                     {
-                        if (!mazeWall[x + 1, y])
+                        for (int i = 0; i < 4; i++)
                         {
-                            for (int i = 0; i < 4; i++)
-                            {
-                                normals.Add(Vector3.left);
-                            }
-                            Vector3[] PlaneV ={
+                            normals.Add(Vector3.forward);
+                        }
+                        Vector3[] PlaneV ={
                                 new Vector3(x+0.5f,1,y+0.5f),
-                                new Vector3(x+0.5f,1,y-0.5f),
+                                new Vector3(x-0.5f,1,y+0.5f),
                                 new Vector3(x+0.5f,0,y+0.5f),
-                                new Vector3(x+0.5f,0,y-0.5f)
+                                new Vector3(x-0.5f,0,y+0.5f)
                             };
-                            vertices.AddRange(PlaneV);
-                            int[] PlaneT ={
+                        vertices.AddRange(PlaneV);
+                        int[] PlaneT ={
                                 trisIndex,trisIndex+1,trisIndex+3,
                                 trisIndex,trisIndex+3,trisIndex+2
                             };
-                            tris.AddRange(PlaneT);
-                            trisIndex += 4;
-                            Vector2[] PlaneU ={
+                        tris.AddRange(PlaneT);
+                        trisIndex += 4;
+                        Vector2[] PlaneU ={
                                 new Vector2(1,1),
                                 new Vector2(0,1),
                                 new Vector2(1,0),
                                 new Vector2(0,0)
                             };
-                            uv.AddRange(PlaneU);
-                        }
+                        uv.AddRange(PlaneU);
                     }
-                    if (x > 0)
+
+                    if (right)
                     {
-                        if (!mazeWall[x - 1, y])
+                        for (int i = 0; i < 4; i++)
                         {
-                            for (int i = 0; i < 4; i++)
-                            {
-                                normals.Add(Vector3.right);
-                            }
-                            Vector3[] PlaneV ={
+                            normals.Add(Vector3.left);
+                        }
+                        Vector3[] PlaneV ={
                                 new Vector3(x-0.5f,1,y+0.5f),
                                 new Vector3(x-0.5f,1,y-0.5f),
                                 new Vector3(x-0.5f,0,y+0.5f),
                                 new Vector3(x-0.5f,0,y-0.5f)
                             };
-                            vertices.AddRange(PlaneV);
-                            int[] PlaneT ={
-                                trisIndex,trisIndex+3,trisIndex+1,
-                                trisIndex,trisIndex+2,trisIndex+3
+                        vertices.AddRange(PlaneV);
+                        int[] PlaneT ={
+                                trisIndex,trisIndex+1,trisIndex+3,
+                                trisIndex,trisIndex+3,trisIndex+2
                             };
-                            tris.AddRange(PlaneT);
-                            trisIndex += 4;
-                            Vector2[] PlaneU ={
+                        tris.AddRange(PlaneT);
+                        trisIndex += 4;
+                        Vector2[] PlaneU ={
                                 new Vector2(1,1),
                                 new Vector2(0,1),
                                 new Vector2(1,0),
                                 new Vector2(0,0)
                             };
-                            uv.AddRange(PlaneU);
-                        }
+                        uv.AddRange(PlaneU);
                     }
-                }
-                else
-                {
+
+                    if (left)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            normals.Add(Vector3.right);
+                        }
+                        Vector3[] PlaneV ={
+                                new Vector3(x+0.5f,1,y+0.5f),
+                                new Vector3(x+0.5f,1,y-0.5f),
+                                new Vector3(x+0.5f,0,y+0.5f),
+                                new Vector3(x+0.5f,0,y-0.5f)
+                            };
+                        vertices.AddRange(PlaneV);
+                        int[] PlaneT ={
+                                trisIndex,trisIndex+3,trisIndex+1,
+                                trisIndex,trisIndex+2,trisIndex+3
+                            };
+                        tris.AddRange(PlaneT);
+                        trisIndex += 4;
+                        Vector2[] PlaneU ={
+                                new Vector2(1,1),
+                                new Vector2(0,1),
+                                new Vector2(1,0),
+                                new Vector2(0,0)
+                            };
+                        uv.AddRange(PlaneU);
+                    }
+
                     for (int i = 0; i < 4; i++)
                     {
                         normals.Add(Vector3.up);
                     }
-                    Vector3[] PlaneV ={
+                    Vector3[] PlaneVP ={
                         new Vector3(x+0.5f,1,y+0.5f),
                         new Vector3(x+0.5f,1,y-0.5f),
                         new Vector3(x-0.5f,1,y+0.5f),
                         new Vector3(x-0.5f,1,y-0.5f)
                     };
-                    vertices.AddRange(PlaneV);
-                    int[] PlaneT ={
+                    vertices.AddRange(PlaneVP);
+                    int[] PlaneTP ={
                         trisIndex,trisIndex+1,trisIndex+3,
                         trisIndex,trisIndex+3,trisIndex+2
                     };
-                    tris.AddRange(PlaneT);
+                    tris.AddRange(PlaneTP);
                     trisIndex += 4;
-                    Vector2[] PlaneU ={
+                    Vector2[] PlaneUP ={
                         new Vector2(1,1),
                         new Vector2(1,0),
                         new Vector2(0,1),
                         new Vector2(0,0)
                     };
-                    uv.AddRange(PlaneU);
+                    uv.AddRange(PlaneUP);
                 }
             }
         }
@@ -252,10 +292,10 @@ public class mazeMesh : MonoBehaviour
         newmesh.uv = uv.ToArray();
 
         MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = newmesh;
         meshFilter.mesh.RecalculateBounds();
         meshFilter.mesh.RecalculateTangents();
-        meshFilter.mesh = newmesh;
-        
+
         GetComponent<MeshCollider>().sharedMesh = newmesh;
 
     }
@@ -302,5 +342,9 @@ public class mazeMesh : MonoBehaviour
             result = direction[(int)Random.Range(0, direction.Count)];
         }
         return result;
+    }
+    void conrolWall()
+    {
+
     }
 }
